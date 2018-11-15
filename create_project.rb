7538035cd95ae022a3c8eb1cc1823f391ef319dd
Christopher Cahill and Phil Mass <christopher@>
2018-11-15 15:15:23 -0800

    adds spec and lib files with class names

--- create_project.rb
+++ create_project.rb
@@ -1,6 +1,7 @@
 require('fileutils')
 
 project_directory = ARGV[0]
+classes = (ARGV[1..-1])
 ARGV.clear
 
 if project_directory
@@ -11,8 +12,17 @@ if project_directory
   FileUtils.mkdir("./lib")
   # FileUtils.touch("./Gemfile")
   file = File.open("./Gemfile", 'w') do |file|
-    file.write("hello")
+    file.write("source 'https://rubygems.org'\n\ngem 'rspec'\ngem 'pivotal_git_scripts'\ngem 'pry'\n")
   end
+  classes.each do |class_name|
+    spec_file = File.open("./spec/#{class_name}_spec.rb", "w") do |file|
+      file.write("require ('rspec')\nrequire ('#{class_name}')\nrequire ('pry')\n")
+    end
+    lib_file = File.open("./lib/#{class_name}.rb", "w") do |file|
+      file.write("require ('pry')\n\n")
+    end
+  end
+  p classes
 else
   puts "Please supply a project directory "
 end
