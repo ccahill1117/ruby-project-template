31b08ed63e21df70f4663a6d205494fa1632ab3a
Christopher Cahill and Phil Mass <phil@>
2018-11-15 16:16:42 -0800

    add camel case names

--- create_project.rb
+++ create_project.rb
@@ -5,24 +5,22 @@ classes = (ARGV[1..-1])
 ARGV.clear
 
 if project_directory
-  puts FileUtils.pwd()
   FileUtils.mkdir("./#{project_directory}")
   FileUtils.cd("./#{project_directory}")
   FileUtils.mkdir("./spec")
   FileUtils.mkdir("./lib")
-  # FileUtils.touch("./Gemfile")
   file = File.open("./Gemfile", 'w') do |file|
     file.write("source 'https://rubygems.org'\n\ngem 'rspec'\ngem 'pivotal_git_scripts'\ngem 'pry'\n")
   end
   classes.each do |class_name|
+    camelName = class_name.gsub(/(_|^)(\w)/){$2.upcase}
     spec_file = File.open("./spec/#{class_name}_spec.rb", "w") do |file|
-      file.write("require ('rspec')\nrequire ('#{class_name}')\nrequire ('pry')\n")
+      file.write("require ('rspec')\nrequire ('#{class_name}')\nrequire ('pry')\n\ndescribe('#{camelName}#_____') do\nend\n")
     end
     lib_file = File.open("./lib/#{class_name}.rb", "w") do |file|
-      file.write("require ('pry')\n\n")
+      file.write("require ('pry')\n\nclass #{camelName}\nend\n")
     end
   end
-  p classes
 else
   puts "Please supply a project directory "
 end
